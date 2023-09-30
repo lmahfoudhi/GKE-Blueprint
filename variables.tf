ce9e6d49ab980780874fe089a5abe4409de609af
leith-mahfoudhi <leith.mahfoudhi@pcp-consulting.fr>
2023-10-01 00:40:13 +0100

    state bucket

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  type    = string
+  default = "us-east1"
+}
+
+variable "project_id" {
+  type    = string
+  default = "devops-catalog-gke"
+}
+variable "state_bucket" {
+  type    = string
+}
